9abed2d2519c04701d02e36cf6c53a65a9659721
JOJO JOHNSON <jojojohnsonibm@gmail.com>
2022-01-28 20:37:05 +0530

    Additions
    All code added to base

---
+++ pkg/routes/bookstore-routes.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/j4real2208/crud-api/pkg/controllers"
+)
+var RegisterBookStoreRoutes = func( router *mux.Router ){
+	router.HandleFunc("/book/",controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/",controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book/{bookId}",controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
+}
